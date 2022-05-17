e8d207994b5ccf9e87f7ef1643c4433e804efeda
Admin <18126037@student.hcmus.edu.vn>
2022-05-17 14:49:14 +0700

    'add'

---
+++ config/connectMySql.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const mysql = require('mysql2')
+
+// const host = mysql.createPool({
+//     host : process.env.HOST,
+//     user : process.env.USER,
+//     database : process.env.DATABASE,
+//     password : process.env.PASSWORD
+// })
+
+const host = mysql.createPool({
+    host : 'tastie-do-user-11494132-0.b.db.ondigitalocean.com',
+    user : 'doadmin',
+    database : 'Tastie',
+    password : 'AVNS_dIQZdxGNQnKqj_q',
+    port : 25060,
+})
+
+module.exports = host.promise();
